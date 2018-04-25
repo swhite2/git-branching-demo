059063182a11e3218141c355f1a063382d4ee562
Stephan de Wit <swit2@avans.nl>
2018-04-25 14:34:24 +0200

    Nieuwe feature

--- server.js
+++ server.js
@@ -5,4 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hallo allemaal')
 })
  
+app.get('/api/helloworld', function (req, res) {
+  res.status(200).json({
+    message: "Hello World!"
+  }).end()
+})
+
 app.listen(3000)
